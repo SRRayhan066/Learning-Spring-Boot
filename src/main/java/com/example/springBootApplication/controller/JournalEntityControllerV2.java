97b227ae0a24a3f78b7aee8978a847b91edb84fc
S R Rayhan <shafikulrahman66@gmail.com>
2024-07-11 13:17:25 +0600

    CRUD app with mongoDB

--- src/main/java/com/example/springBootApplication/controller/JournalEntityControllerV2.java
+++ src/main/java/com/example/springBootApplication/controller/JournalEntityControllerV2.java
@@ -3,10 +3,9 @@ package com.example.springBootApplication.controller;
 import com.example.springBootApplication.entity.JournalEntity;
 import com.example.springBootApplication.service.JournalService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/journalV2")
@@ -18,5 +17,25 @@ public class JournalEntityControllerV2 {
     public String createAJournal(@RequestBody JournalEntity journalEntity){
         journalService.save(journalEntity);
         return "Successful";
+    }
+
+    @GetMapping
+    public List<JournalEntity> getAllJournal(){
+        return journalService.getAll();
+    }
+
+    @GetMapping("/id")
+    public JournalEntity getAJournal(@RequestParam("id") String id){
+        return journalService.getAJournal(id).orElse(null);
+    }
+
+    @PutMapping("/update")
+    public JournalEntity updateAJournal(@RequestParam("id") String id,@RequestBody JournalEntity journal){
+        return journalService.updateOne(id,journal);
+    }
+
+    @DeleteMapping("/delete")
+    public String deleteOne(@RequestParam("id") String id){
+        return journalService.deleteOne(id);
     }
 }
--- src/main/java/com/example/springBootApplication/service/JournalService.java
+++ src/main/java/com/example/springBootApplication/service/JournalService.java
@@ -5,6 +5,9 @@ import com.example.springBootApplication.repository.JournalServiceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.Optional;
+
 @Component
 public class JournalService {
     @Autowired
@@ -13,4 +16,28 @@ public class JournalService {
     public void save(JournalEntity journalEntity){
         journalServiceRepository.save(journalEntity);
     }
+
+    public List<JournalEntity> getAll(){
+        return journalServiceRepository.findAll();
+    }
+
+    public Optional<JournalEntity> getAJournal(String id){
+        return journalServiceRepository.findById(id);
+    }
+
+    public JournalEntity updateOne(String id,JournalEntity journal){
+        JournalEntity oldJournal = journalServiceRepository.findById(id).orElse(null);
+        if(oldJournal!=null){
+            oldJournal.setTitle(journal.getTitle()!=null && !journal.getTitle().equals("") ? journal.getTitle() : oldJournal.getTitle());
+            oldJournal.setContent(journal.getContent()!=null && !journal.getContent().equals("") ? journal.getContent() : oldJournal.getContent());
+        }
+        journalServiceRepository.save(oldJournal);
+        return oldJournal;
+    }
+
+    public String deleteOne(String id){
+        journalServiceRepository.deleteById(id);
+        return "Successful";
+    }
+
 }
