5d9abd97b888686278e19e8dd3940a0a9fde95b6
S R Rayhan <shafikulrahman66@gmail.com>
2024-07-16 22:07:50 +0600

    Lombok integrated

--- src/main/java/com/example/springBootApplication/entity/JournalEntity.java
+++ src/main/java/com/example/springBootApplication/entity/JournalEntity.java
@@ -1,38 +1,16 @@
 package com.example.springBootApplication.entity;
 
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Collection;
 
+@Data
 @Document(collection = "journal_entries")
 public class JournalEntity {
     @Id
     String id;
     String title;
     String content;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 }
